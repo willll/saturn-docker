e6a5110254d17bb7ba4c3e6ce4e29a4f875957c5
Wilfried Fauvel <wilfried.fauvel@gmail.com>
2021-12-12 21:15:47 -0500

    Add files via upload

--- Resources/build-samples.sh
+++ Resources/build-samples.sh
@@ -3,12 +3,27 @@ if [ ! -d $SATURN_SAMPLES ]; then
 	mkdir -p $SATURN_SAMPLES
 fi
 
+# Settings
+build_samples=1
+make_ISO=0
+
 cd $SATURN_SAMPLES
 
-# build segalib
-mkdir -p $SATURN_SAMPLES/bin
-cmake -S $SATURN_SAMPLES/ -B $SATURN_SAMPLES/bin/ \
-		-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
-		-DCMAKE_INSTALL_PREFIX=$SATURN_SAMPLES
-make -f $SATURN_SAMPLES/bin/Makefile -C $SATURN_SAMPLES/bin/ && \
- 		make -f $SATURN_SAMPLES/bin/Makefile -C $SATURN_SAMPLES/bin/ install
+#
+# build samples
+#
+if [ $build_samples -eq 1 ]; then
+	mkdir -p $SATURN_SAMPLES/bin
+	cmake -S $SATURN_SAMPLES/ -B $SATURN_SAMPLES/bin/ \
+			-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
+			-DCMAKE_INSTALL_PREFIX=$SATURN_SAMPLES
+	make -f $SATURN_SAMPLES/bin/Makefile -C $SATURN_SAMPLES/bin/ && \
+	 		make -f $SATURN_SAMPLES/bin/Makefile -C $SATURN_SAMPLES/bin/ install
+fi
+
+#
+# make ISOs
+#
+if [ $make_ISO -eq 1 ]; then
+	mkdir -p ${SATURN_SAMPLES}/CD
+fi
--- Resources/build-sbl6.sh
+++ Resources/build-sbl6.sh
@@ -3,6 +3,10 @@ if [ ! -d $SATURN_TMP ]; then
 	mkdir -p $SATURN_TMP
 fi
 
+# Settings
+build_segalib=1
+build_segasmp=1
+
 cd $SATURN_TMP
 
 # diff -crB --binary sbl6/ sbl6_patch/ > sbl6.patch
@@ -17,39 +21,45 @@ rm $SATURN_TMP/sbl6/segalib/spr/sega_spr.h
 # Inject Build System
 cp -r $SATURN_TMP/sbl6_/* $SATURN_TMP/sbl6/
 
+#
 # build segalib
-mkdir -p $SATURN_TMP/sbl6/segalib/bin
-cmake -S $SATURN_TMP/sbl6/segalib/ -B $SATURN_TMP/sbl6/segalib/bin/ \
-		-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
-		-DCMAKE_INSTALL_PREFIX=$SATURN_SBL
-make -f $SATURN_TMP/sbl6/segalib/bin/Makefile -C $SATURN_TMP/sbl6/segalib/bin/ && \
- 		make -f $SATURN_TMP/sbl6/segalib/bin/Makefile -C $SATURN_TMP/sbl6/segalib/bin/ install
+#
+if [ $build_segalib -eq 1 ]; then
+	mkdir -p $SATURN_TMP/sbl6/segalib/bin
+	cmake -S $SATURN_TMP/sbl6/segalib/ -B $SATURN_TMP/sbl6/segalib/bin/ \
+			-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
+			-DCMAKE_INSTALL_PREFIX=$SATURN_SBL
+	make -f $SATURN_TMP/sbl6/segalib/bin/Makefile -C $SATURN_TMP/sbl6/segalib/bin/ && \
+	 		make -f $SATURN_TMP/sbl6/segalib/bin/Makefile -C $SATURN_TMP/sbl6/segalib/bin/ install
+fi
 
 #
 # build segasmp
 #
-
-mkdir -p ${SATURN_TMP}/sbl6/segasmp/bin
-mkdir -p ${SATURN_SBL}/segasmp/bup/CD
-cmake -S $SATURN_TMP/sbl6/segasmp/ -B $SATURN_TMP/sbl6/segasmp/bin/ \
-		-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
-		-DCMAKE_INSTALL_PREFIX=$SATURN_SBL
-make -f $SATURN_TMP/sbl6/segasmp/bin/Makefile -C $SATURN_TMP/sbl6/segasmp/bin/ && \
- 		make -f $SATURN_TMP/sbl6/segasmp/bin/Makefile -C $SATURN_TMP/sbl6/segasmp/bin/ install
+if [ $build_segasmp -eq 1 ]; then
+	mkdir -p ${SATURN_TMP}/sbl6/segasmp/bin
+	mkdir -p ${SATURN_SBL}/segasmp/bup/CD
+	cmake -S $SATURN_TMP/sbl6/segasmp/ -B $SATURN_TMP/sbl6/segasmp/bin/ \
+			-DCMAKE_TOOLCHAIN_FILE=$SATURN_CMAKE/sega_saturn.cmake \
+			-DCMAKE_INSTALL_PREFIX=$SATURN_SBL
+	make -f $SATURN_TMP/sbl6/segasmp/bin/Makefile -C $SATURN_TMP/sbl6/segasmp/bin/ && \
+	 		make -f $SATURN_TMP/sbl6/segasmp/bin/Makefile -C $SATURN_TMP/sbl6/segasmp/bin/ install
+fi
 
 #
 # Create ISO file
 #
+if [ $build_segasmp -eq 1 ]; then
+	# BUP
+	cp $SATURN_SBL/segasmp/bup/bup.bin $SATURN_SBL/segasmp/bup/CD/0.bin
+	cp $SATURN_CD/{CPY.txt,BIB.txt,ABS.txt} $SATURN_SBL/segasmp/bup/CD/
+	cp $SATURN_TMP/sbl6/segasmp/bup/IP.BIN $SATURN_SBL/segasmp/bup/CD/
+	cp $SATURN_TMP/sbl6/segasmp/bup/bup.cue $SATURN_SBL/segasmp/bup/
+	cp $SATURN_TMP/sbl6/segasmp/bup/cd_included_files.txt $SATURN_SBL/segasmp/bup/CD/FILES.txt
+	cp $SATURN_TMP/sbl6/segasmp/bup/cd_excludes_files.txt $SATURN_SBL/segasmp/bup/CD/EXCLUDES.txt
 
-# BUP
-cp $SATURN_SBL/segasmp/bup/bup $SATURN_SBL/segasmp/bup/CD/0.bin
-cp $SATURN_CD/{CPY.txt,BIB.txt,ABS.txt} $SATURN_SBL/segasmp/bup/CD/
-cp $SATURN_TMP/sbl6/segasmp/bup/IP.BIN $SATURN_SBL/segasmp/bup/CD/
-cp $SATURN_TMP/sbl6/segasmp/bup/bup.cue $SATURN_SBL/segasmp/bup/
-cp $SATURN_TMP/sbl6/segasmp/bup/cd_included_files.txt $SATURN_SBL/segasmp/bup/CD/FILES.txt
-cp $SATURN_TMP/sbl6/segasmp/bup/cd_excludes_files.txt $SATURN_SBL/segasmp/bup/CD/EXCLUDES.txt
-
-cd $SATURN_SBL/segasmp/bup/CD/ && \
-$SATURN_CD/create_cd.sh "bup" "Kenshiro" "Raoh" \
-				"IP.BIN" \
- 				$SATURN_SBL/segasmp/bup/bup.iso
+	cd $SATURN_SBL/segasmp/bup/CD/ && \
+	$SATURN_CD/create_cd.sh "bup" "Kenshiro" "Raoh" \
+					"IP.BIN" \
+	 				$SATURN_SBL/segasmp/bup/bup.iso
+fi
