e0db2a7371180ff9746c35590951c2e24325c57d
willll <wilfried.fauvel@gmail.com>
2022-02-05 20:48:41 -0500

    Added IPMaker build script

--- Resources/build-IPMaker.sh
+++ Resources/build-IPMaker.sh
@@ -5,19 +5,17 @@ if [ ! -d $SATURN_IPMAKER ]; then
 	mkdir -p $SATURN_IPMAKER
 fi
 
-cd $SATURN_TMP
-
 #
 # build IPMaker
 #
-mkdir -p $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin
+mkdir -p $SATURN_TMP/bin
 
-cmake -S $SATURN_TMP/Saturn-SDK-Tool-IPMaker/ \
-			-B $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin \
-			-DCMAKE_INSTALL_PREFIX=$SATURN_IPMAKER
-make -f $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin/Makefile \
-			-C $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin/ && \
-make -f $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin/Makefile \
-			-C $SATURN_TMP/Saturn-SDK-Tool-IPMaker/bin/ install
+cmake -S $SATURN_TMP/ \
+			-B $SATURN_TMP/bin \
+			-DCMAKE_INSTALL_PREFIX=$SATURN_ROOT
+make -f $SATURN_TMP/bin/Makefile \
+			-C $SATURN_TMP/bin/ && \
+make -f $SATURN_TMP/bin/Makefile \
+			-C $SATURN_TMP/bin/ install
 
 exit 0
