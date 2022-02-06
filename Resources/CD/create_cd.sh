2b76daea65e5249a4213990b419cad5383fb4e15
willll <wilfried.fauvel@gmail.com>
2022-02-05 20:49:30 -0500

    Clean up CD resources

--- Resources/CD/create_cd.sh
+++ Resources/CD/create_cd.sh
@@ -3,12 +3,17 @@ set -e
 
 echo "Generating ISO ------------------>  $1"
 
-$SATURN_CD/mkisofs -v -sysid "SEGA SEGASATURN" -volid $1 -volset $1 -publisher $2 \
- -preparer $3 -appid "CD0001" \
- -abstract "ABS.TXT" -copyright "CPY.TXT" -biblio "BIB.TXT" \
- -generic-boot $4 \
- -full-iso9660-filenames -o $5 \
- -exclude-list EXCLUDES.txt \
- -path-list FILES.txt
+echo $SATURN_CD/ABS.TXT >> $6/CD/FILES.txt
+echo $SATURN_CD/BIB.TXT >> $6/CD/FILES.txt
+echo $SATURN_CD/CPY.TXT >> $6/CD/FILES.txt
+echo "$6/CD/$4" >> $6/CD/FILES.txt
+
+$SATURN_CD/mkisofs -v -sysid "SEGA SEGASATURN" -volid "$1" -volset "$1"  -publisher "$2" \
+ -preparer "$3" -appid "$1" -sectype 2352 \
+ -abstract "$SATURN_CD/ABS.TXT" -copyright "$SATURN_CD/CPY.TXT" -biblio "$SATURN_CD/BIB.TXT" \
+ -generic-boot "$6CD/$4" \
+ -full-iso9660-filenames -o "$6/$5" \
+ -exclude-list $6/CD/EXCLUDES.txt \
+ -path-list $6/CD/FILES.txt
 
 exit
