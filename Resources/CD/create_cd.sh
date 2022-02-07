4b6a8304a19f733f210c3152de90ac878fbbd94b
willll <wilfried.fauvel@gmail.com>
2022-02-07 11:31:36 -0500

    Fixed filename

--- Resources/CD/create_cd.sh
+++ Resources/CD/create_cd.sh
@@ -14,7 +14,7 @@ fi
 
 $SATURN_CD/mkisofs -v -sysid "SEGA SEGASATURN" -volid "$1" -volset "$1"  -publisher "$2" \
  -preparer "$3" -appid "$1" -sectype 2352 \
- -abstract "$SATURN_CD/ABS.TXT" -copyright "$SATURN_CD/CPY.TXT" -biblio "$SATURN_CD/BIB.TXT" \
+ -abstract "ABS.TXT" -copyright "CPY.TXT" -biblio "BIB.TXT" \
  -generic-boot "$6CD/$4" \
  -full-iso9660-filenames -o "$6/$5" \
  -exclude-list "$6/CD/EXCLUDES.txt" \
