953adaf49811937e6e088c32b27b5a4e314d3092
willll <wilfried.fauvel@gmail.com>
2022-02-20 21:12:32 -0500

    tuned up git traffic

--- Resources/dl-SaturnSDK-samples.sh
+++ Resources/dl-SaturnSDK-samples.sh
@@ -3,15 +3,15 @@ set -e
 
 if [ $INSTALL_SATURNSDK_SAMPLES -eq 1 ]; then
 
-	git clone https://github.com/SaturnSDK/Saturn-SDK-Sample-00-Basic.git \
+	git clone --depth 1 https://github.com/SaturnSDK/Saturn-SDK-Sample-00-Basic.git \
 	    "$SATURN_SAMPLES"/Saturn-SDK-Sample-00-Basic
-	git clone https://github.com/SaturnSDK/Saturn-SDK-Sample-01-Sprite.git \
+	git clone --depth 1 https://github.com/SaturnSDK/Saturn-SDK-Sample-01-Sprite.git \
 	    "$SATURN_SAMPLES"/Saturn-SDK-Sample-01-Sprite
-	git clone https://github.com/SaturnSDK/Saturn-SDK-Sample-02-TexturedSprite.git \
+	git clone --depth 1 https://github.com/SaturnSDK/Saturn-SDK-Sample-02-TexturedSprite.git \
 	    "$SATURN_SAMPLES"/Saturn-SDK-Sample-02-TexturedSprite
-	git clone https://github.com/SaturnSDK/Saturn-SDK-Sample-Common.git \
+	git clone --depth 1 https://github.com/SaturnSDK/Saturn-SDK-Sample-Common.git \
 	    "$SATURN_SAMPLES"/Saturn-SDK-Sample-Common
-	git clone https://github.com/SaturnSDK/Saturn-SDK-Sample-13-CDBrowser.git \
+	git clone --depth 1 https://github.com/SaturnSDK/Saturn-SDK-Sample-13-CDBrowser.git \
 	    "$SATURN_SAMPLES"/Saturn-SDK-Sample-13-CDBrowser
 
 fi
