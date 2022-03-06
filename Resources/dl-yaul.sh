d8edfd1bb5e558fa4061bff9abb5e0e6c62aab77
willll <wilfried.fauvel@gmail.com>
2022-03-06 17:28:21 -0500

    Fixed errors when Yaul is disabled

--- Resources/dl-yaul.sh
+++ Resources/dl-yaul.sh
@@ -9,6 +9,14 @@ if [ $INSTALL_YAUL_LIB -eq 1 ]; then
 
   git clone --depth 1 https://github.com/ijacquez/libyaul.git "$SATURN_TMP/yaul"
 
+else
+
+	# create temp directories to avoid Docker errors
+	mkdir -p "$SATURN_TMP/yaul/libyaul/common/ldscripts/"
+	mkdir -p "$SATURN_TMP/yaul/libyaul/common/specs/"
+	mkdir -p "$SATURN_TMP/yaul/tools/bin2o/"
+	mkdir -p "$SATURN_TMP/yaul/tools/make-ip/"
+
 fi
 
 exit 0
