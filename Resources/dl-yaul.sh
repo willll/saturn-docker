9ed74da3dce34f7fdf0ab8686e92dbf4b1d6799d
Tiago Rezende <tiagosr@gmail.com>
2022-06-16 19:04:26 +0200

    Specifying fixed revisions for Jo Engine and YAUL

--- Resources/dl-yaul.sh
+++ Resources/dl-yaul.sh
@@ -7,7 +7,10 @@ if [ $INSTALL_YAUL_LIB -eq 1 ]; then
 		mkdir -p $SATURN_TMP
 	fi
 
-  git clone --depth 1 https://github.com/ijacquez/libyaul.git "$SATURN_TMP/yaul"
+  git clone -n https://github.com/ijacquez/libyaul.git "$SATURN_TMP/yaul"
+  pushd "$SATURN_TMP/yaul"
+  git checkout $YAUL_COMMIT_SHA
+  popd
 
 else
 
