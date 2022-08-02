e39b884e0b37316024f441c87c7350ee35d9d63a
willll <wilfried.fauvel@gmail.com>
2022-08-02 14:45:38 -0400

    Fixed Yaul version to 1.0.1

--- Resources/dl-yaul.sh
+++ Resources/dl-yaul.sh
@@ -7,10 +7,7 @@ if [ $INSTALL_YAUL_LIB -eq 1 ]; then
 		mkdir -p $SATURN_TMP
 	fi
 
-  git clone -n https://github.com/ijacquez/libyaul.git "$SATURN_TMP/yaul"
-  pushd "$SATURN_TMP/yaul"
-  git checkout $YAUL_COMMIT_SHA
-  popd
+  git clone -b $YAUL_TAG --depth 1 https://github.com/ijacquez/libyaul.git "$SATURN_TMP/yaul"
 
 else
 
