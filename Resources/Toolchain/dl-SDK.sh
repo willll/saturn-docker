810316d4e30211714586f9ac9882ffa99aa16700
willll <wilfried.fauvel@gmail.com>
2022-08-27 18:34:23 -0400

    Set GCC version as a parameter

--- Resources/Toolchain/dl-SDK.sh
+++ Resources/Toolchain/dl-SDK.sh
@@ -2,7 +2,7 @@
 set -e
 
 #git clone --depth 1 --branch gcc_$GCC_VERSION \
-git clone --depth 1 --branch master \
+git clone --depth 1 --branch "gcc_$GCC_VERSION" \
     https://github.com/willll/Saturn-SDK-GCC-SH2.git "$BUILD_FOLDER"
 
 chmod 777 "$BUILD_FOLDER"/*.sh
