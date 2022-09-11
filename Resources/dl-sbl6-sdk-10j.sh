ad2f90ecedc7e3b97a05a8f09966d3cfac922397
willll <wilfried.fauvel@gmail.com>
2022-09-11 07:12:01 -0400

    Use gdown to down from gdrive

--- Resources/dl-sbl6-sdk-10j.sh
+++ Resources/dl-sbl6-sdk-10j.sh
@@ -7,9 +7,7 @@ if [ $INSTALL_SBL_LIB -eq 1 ]; then
 		mkdir -p $SATURN_TMP
 	fi
 
-	curl -s -o nul -c ./cookie-file1 -s -L "https://drive.google.com/file/d/1wqc7bngPUOv_0hgyuIyCTq8l4jc_azSN/view?usp=sharing"& \
-	curl -Lb ./cookie-file1 "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' \
-	 ./cookie-file1`&id=1wqc7bngPUOv_0hgyuIyCTq8l4jc_azSN" -o $SATURN_TMP/sdk_10j.zip
+	gdown 1wqc7bngPUOv_0hgyuIyCTq8l4jc_azSN -O "$SATURN_TMP/sdk_10j.zip"
 
 	if [ ! -f "$SATURN_TMP/sdk_10j.zip" ]; then
 	    echo "$SATURN_TMP/sdk_10j.zip not downloaded."
