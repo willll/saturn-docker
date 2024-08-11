1508199c642ba726b150e7b82101680a981773b3
willll <464311+willll@users.noreply.github.com>
2024-08-11 10:32:46 -0400

    WIP

--- Resources/Toolchain/build-SDK-elf.sh
+++ Resources/Toolchain/build-SDK-elf.sh
@@ -9,7 +9,12 @@ export PROGRAM_PREFIX=sh-elf-
 export TARGETMACH=sh-elf
 export OBJFORMAT=ELF
 export GCC_BOOTSTRAP="--disable-bootstrap"
+export ROOTDIR="${BUILD_FOLDER}"
 export ENABLE_DOWNLOAD_CACHE=0
+
+if [ "$GCC_VERSION"  == "14.1.0" ]; then
+	export ENABLE_DOWNLOAD_CACHE=1
+fi
 
 cd "${BUILD_FOLDER}"
 
