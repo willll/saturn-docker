c8ec3300267474cd2547a89d55f194ceac064e0b
willll <464311+willll@users.noreply.github.com>
2025-01-12 14:26:57 -0500

    Added new GCC build variable

--- Resources/Toolchain/build-SDK-elf.sh
+++ Resources/Toolchain/build-SDK-elf.sh
@@ -11,6 +11,7 @@ export OBJFORMAT=ELF
 export GCC_BOOTSTRAP="--disable-bootstrap"
 export ROOTDIR="${BUILD_FOLDER}"
 export ENABLE_DOWNLOAD_CACHE=0
+export ENABLE_STATIC_BUILD=0
 
 if [ "$GCC_VERSION"  == "14.1.0" ]; then
 	export ENABLE_DOWNLOAD_CACHE=1
