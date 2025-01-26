06d478467726f7b831173f5a3fbfc91aa3ca7b8d
willll <464311+willll@users.noreply.github.com>
2025-01-26 14:28:00 -0500

    Cleanup build-SDK-elf.sh by removing commented-out variables

--- Resources/Toolchain/build-SDK-elf.sh
+++ Resources/Toolchain/build-SDK-elf.sh
@@ -8,12 +8,7 @@ fi
 export PROGRAM_PREFIX=sh-elf-
 export TARGETMACH=sh-elf
 export OBJFORMAT=ELF
-#export GCC_BOOTSTRAP="--disable-bootstrap"
 export ROOTDIR="${BUILD_FOLDER}"
-#export ENABLE_DOWNLOAD_CACHE=0
-#export ENABLE_STATIC_BUILD=0
-#export ENABLE_BOOTSTRAP=0
-
 
 cd "${BUILD_FOLDER}"
 
